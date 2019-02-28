1ecd20024e0332bc1c1b8807b54920cd8472627a
平林雅士 <masashi@MacBook-Pro-masashi.local>
2019-02-28 23:18:27 +0900

    learn State and setState

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    const greeting = "HI";
-    const dom = <h1>{greeting}</h1>
+const App = () => (<Counter></Counter>)
 
+class Counter extends Component {
+  constructor(props){
+    super(props)
+    this.state = { count: 0 }
+  }
+
+  handlePlusButton = () => {
+    this.setState({count: this.state.count + 1 })
+  }
+
+  handleMinusButton = () => {
+    this.setState({count: this.state.count -1})
+  }
+
+  render(){
+    console.log(this.state)
     return (
       <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => {console.log("Hi!")}} />
-        </React.Fragment>
+    <div>count: {this.state.count}</div>
+    <button onClick={this.handlePlusButton}>+1</button>
+    <button onClick={this.handleMinusButton}>-1</button>
+    </React.Fragment>
     );
+
   }
 }
+
 
 export default App;
